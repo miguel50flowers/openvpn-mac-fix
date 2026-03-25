e3b8fac08716042d5ce31cfe2714ca3f86b9727e
Miguel Angel Esparza Calero <miguel@fiftyflowers.com>
2026-03-25 12:38:15 -0500

    Bump version to 4.3.0

--- Formula/openvpn-mac-fix.rb
+++ Formula/openvpn-mac-fix.rb
@@ -1,7 +1,7 @@
 class OpenvpnMacFix < Formula
   desc "Automatic fix for internet loss after disconnecting OpenVPN on macOS"
   homepage "https://vpn-fix.maecly.com/"
-  url "https://github.com/miguel50flowers/openvpn-mac-fix/archive/refs/tags/v4.2.1.tar.gz"
+  url "https://github.com/miguel50flowers/openvpn-mac-fix/archive/refs/tags/v4.3.0.tar.gz"
   sha256 ""
   license "MIT"
 
